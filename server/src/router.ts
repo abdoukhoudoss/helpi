a54265c5e8d1d3e1a2748a716fbd33257e38719f
JustineC31 <justine-cousin@hotmail.com>
2024-12-05 15:54:46 +0100

    commit justine

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,10 +8,13 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
+import missionAction from "./modules/item/mission/missionAction";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+router.get("/api/missions", missionAction.browse);
 
 /* ************************************************************************* */
 
