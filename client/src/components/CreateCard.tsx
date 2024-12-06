99382fa0bb515b6dd0f344084ba4330b1b4722aa
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-12-06 11:49:06 +0100

    search-bar

--- client/src/components/CreateCard.tsx
+++ client/src/components/CreateCard.tsx
@@ -1,16 +1,5 @@
-function createCard(house) {
-    const template = `<div class="card">
-    <div class="card-header">
-      <div class="card-img" style="background-image:url('${house.img}') "></div>
-    </div>
-  <div class="card-body">
-    <h2 class="card-title">${house.name}</h2>
-    <p class="card-description">${house.desc}</p>
-  <button class="card-button">I want it!</button>
-  </div>
-  </div>`;
+function createCard() {
   
-    return template;
   }
   
   export default createCard;
